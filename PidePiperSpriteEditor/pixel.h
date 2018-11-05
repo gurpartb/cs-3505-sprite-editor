7580a148318621722f7861ce89beb47de68837c4
gurpartb <gurpartap.s.bhatti@gmail.com>
2018-11-04 17:03:44 -0700

    Created all the classes for Sprite Editor.

---
+++ PidePiperSpriteEditor/pixel.h
@@ -0,0 +1,11 @@
+#ifndef PIXEL_H
+#define PIXEL_H
+
+
+class Pixel
+{
+public:
+    Pixel();
+};
+
+#endif // PIXEL_H
---
+++ PidePiperSpriteEditor/spriteframe.h
@@ -0,0 +1,11 @@
+#ifndef SPRITEFRAME_H
+#define SPRITEFRAME_H
+
+
+class SpriteFrame
+{
+public:
+    SpriteFrame();
+};
+
+#endif // SPRITEFRAME_H
